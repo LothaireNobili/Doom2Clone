1eba34015263aedc0ea616610f9099e22bf813b1
LothaireNobili <lothairenobili@gmail.com>
2024-10-14 15:24:53 +0200

    initiated main.py

---
+++ source/main.py
@@ -0,0 +1,21 @@
+from settings import *
+from engine import Engine
+
+class Game:
+    ray.init_window(WIN_WIDTH, WIN_HEIGHT, "Doom 2 clone")
+
+    def __init__(self):
+        self.dt = 0.0
+        self.engine = Engine(game=self)
+
+    def run(self):
+        while not ray.window_should_close():
+            self.dt = ray.get_frame_time()
+            self.engine.update()
+            self.engine.draw()
+        
+        ray.close_window()
+
+if __name__ == "__main__":
+    game = Game()
+    game.run()
