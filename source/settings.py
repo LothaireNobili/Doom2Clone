12f545ad59406424ae4f77dfe0f26157a9ec776a
LothaireNobili <lothairenobili@gmail.com>
2024-10-14 15:09:17 +0200

    initiliazed settings.py (used for configuration and options)

---
+++ source/settings.py
@@ -0,0 +1,7 @@
+#configuration and options of the game
+import pyray as ray
+import glm
+from glm import vec2, vec3, ivec2, normalize, cross, dot, atan2, sin, cos, length #common function we explicitely import to make the code easier to read
+
+#resolution
+WIN_RES = WIN_WIDTH, WIN_HEIGHT = 1600, 900
